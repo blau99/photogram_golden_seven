8eed6477aab32427fe8e7de3423f15388d3a854a
blau99 <blau99@users.noreply.github.com>
2017-02-05 14:36:33 -0600

    made changes

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,33 @@ class PhotosController < ApplicationController
   end
 
 
+  def show
+    p = Photo.find(params[:id])
+    @source = p.source
+    @id = p.id
+    @caption = p.caption
+    render("photos/show.html.erb")
+  end
+
+  def new_form
+
+    render("photos/new_form.html.erb")
+  end
+
+  def edit_form
+    p = Photo.find(params[:id])
+    @source = p.source
+    @id = p.id
+    @caption = p.caption
+    render("photos/edit_form.html.erb")
+  end
+
+  def update_row
+    p = Photo.find(params[:id])
+    @id = p.id
+    p.source = @source
+    p.caption = @caption
+    p.save
+    render("photos/show.html.erb")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
+  get("/photos/new", { :controller => "photos", :action => "new_form" })
+  get("/photos/delete_photo/:id", { :controller => "photos", :action => "delete_photo"})
+  get("/photos/:id/edit_form", { :controller => "photos", :action => "edit_form"})
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+  get("/photos/update_photo/:id", { :controller => "photos", :action => "update_row" })
+
 
 end
